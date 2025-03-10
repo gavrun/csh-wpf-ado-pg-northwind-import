6469a04cda52f7c945b5ee4deb009db7071db3e1
AG <38539655+gavrun@users.noreply.github.com>
2025-03-10 12:33:59 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace csh_wpf_ado_pg_northwind_import;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
