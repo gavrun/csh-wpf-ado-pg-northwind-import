d0cc88db7c0c340d15e4e1b671b6d8452299c414
AG <38539655+gavrun@users.noreply.github.com>
2025-03-13 06:12:00 +0000

    added query helper, mapped data to contols products orders customers

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,9 +34,13 @@ public partial class MainWindow : Window
 
         ConnectToDefault();
 
-        //LoadProducts();
-        //LoadOrders(); 
-        //LoadCustomers();
+        LoadCategories();
+        LoadProducts();
+
+        LoadCustomersFilter();
+        LoadOrders(); 
+
+        LoadCustomers();
     }
     
     // read dbconfig.xml when starting 
@@ -119,8 +123,8 @@ public partial class MainWindow : Window
             App.SetActiveConnection(connection);
 
             // DEBUG
-            string conn = selectedConnection.ConnectionName.ToString();
-            MessageBox.Show($"Connecting to {conn}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            //string conn = selectedConnection.ConnectionName.ToString();
+            //MessageBox.Show($"Connecting to {conn}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
 
             // push status to UI
             //ConnectionStatus.Text = $"Status: Connected to {selectedConnection.ConnectionName}";
@@ -134,6 +138,139 @@ public partial class MainWindow : Window
             //throw;
         }
     }
+
+    // TBD DB workflow
+
+    // TBD adjust width based on content ColumnWidth="*" 
+    private void AutoAdjustColumnWidths(DataGrid dataGrid)
+    {
+        if (dataGrid != null)
+        {
+            foreach (var column in dataGrid.Columns)
+            {
+                column.Width = DataGridLength.Auto;
+                //column.Width = DataGridLength.SizeToCells; 
+            }
+        }
+    }
+
+    private void LoadCategories()
+    {
+        string query = "SELECT category_id, category_name FROM categories;";
+
+        var categories = QueryHelper.ExecuteQuery(query, reader => new
+        {
+            CategoryId = reader.GetInt32(0),
+            CategoryName = reader.GetString(1)
+        });
+
+        CategoryFilter.ItemsSource = categories;
+        CategoryFilter.DisplayMemberPath = "CategoryName";
+        CategoryFilter.SelectedValuePath = "CategoryId";
+
+        // default value
+        CategoryFilter.SelectedIndex = -1;
+    }
+
+    private void LoadProducts(int? categoryId = null)
+    {
+        string query = "SELECT product_id, product_name, unit_price, units_in_stock, category_id, supplier_id FROM products";
+
+        var parameters = new Dictionary<string, object>();
+
+        if (categoryId.HasValue)
+        {
+            query += " WHERE category_id = @category";
+
+            parameters.Add("@category", categoryId.Value);
+        }
+
+        var products = QueryHelper.ExecuteQuery(query, reader => new
+        {
+            ProductId = reader.GetInt32(0),
+            ProductName = reader.GetString(1),
+            UnitPrice = reader.GetFloat(2), // ISSUE check type real to float
+            UnitsInStock = reader.GetInt16(3),
+            CategoryId = reader.GetInt32(4),
+            SupplierId = reader.GetInt32(5)
+        }, 
+        parameters);
+
+        ProductsGrid.ItemsSource = products;
+
+        //AutoAdjustColumnWidths(ProductsGrid);
+    }
+
+    private void LoadCustomersFilter()
+    {
+        string query = "SELECT DISTINCT customer_id FROM orders WHERE customer_id IS NOT NULL";
+
+        var customers = QueryHelper.ExecuteQuery(query, reader => reader.GetString(0));
+
+        customers.Insert(0, "All Customers"); // item all customers for filtering 
+
+        CustomerFilter.ItemsSource = customers;
+        CustomerFilter.SelectedIndex = 0;
+    }
+
+    private void LoadOrders(string? customerId = null, bool lastMonthOnly = false)
+    {
+        string query = "SELECT order_id, customer_id, order_date, ship_country FROM orders";
+
+        var parameters = new Dictionary<string, object>();
+
+        List<string> conditions = new List<string>();
+
+        if (!string.IsNullOrEmpty(customerId))
+        {
+            conditions.Add("customer_id = @customerId");
+            parameters["@customerId"] = customerId;
+        }
+
+        if (lastMonthOnly)
+        {
+            conditions.Add("order_date >= NOW() - INTERVAL '1 month'");
+        }
+
+        if (conditions.Count > 0)
+        {
+            query += " WHERE " + string.Join(" AND ", conditions);
+        }
+
+        var orders = QueryHelper.ExecuteQuery(query, reader => new
+        {
+            OrderId = reader.GetInt32(0),
+            CustomerId = reader.IsDBNull(1) ? "N/A" : reader.GetString(1),
+            OrderDate = reader.IsDBNull(2) ? (DateTime?)null : reader.GetDateTime(2),
+            ShipCountry = reader.IsDBNull(3) ? "N/A" : reader.GetString(3)
+        }, 
+        parameters);
+
+        OrdersGrid.ItemsSource = orders;
+    }
+
+    private void LoadCustomers(string? country = null)
+    {
+        string query = "SELECT customer_id, company_name, contact_name, country FROM customers";
+        var parameters = new Dictionary<string, object>();
+
+        if (!string.IsNullOrEmpty(country))
+        {
+            query += " WHERE country ILIKE @country";
+            parameters["@country"] = $"%{country}%";
+        }
+
+        var customers = QueryHelper.ExecuteQuery(query, reader => new
+        {
+            CustomerId = reader.GetString(0),
+            CompanyName = reader.GetString(1),
+            ContactName = reader.IsDBNull(2) ? "N/A" : reader.GetString(2),
+            Country = reader.IsDBNull(3) ? "N/A" : reader.GetString(3)
+        }, parameters);
+
+        CustomersGrid.ItemsSource = customers;
+    }
+
 
     // TBD ETL workflow
     private void ProcessCsvFile(string filePath)
@@ -248,12 +385,18 @@ public partial class MainWindow : Window
     {
         if (CategoryFilter.SelectedItem != null)
         {
-            string selectedCategory = CategoryFilter.SelectedItem.ToString();
-
-            MessageBox.Show($"Filtering by {selectedCategory}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            // DEBUG
+            //string selectedCategory = CategoryFilter.SelectedItem.ToString();
+            //MessageBox.Show($"Filtering by {selectedCategory}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
 
             // load data from database
             // SQL query: SELECT * FROM Products WHERE Category = selectedCategory
+            int selectedCategory = (int)CategoryFilter.SelectedValue;
+            LoadProducts(selectedCategory);
+        }
+        else
+        {
+            LoadProducts();
         }
     }
 
@@ -275,11 +418,19 @@ public partial class MainWindow : Window
         if (CustomerFilter.SelectedItem != null)
         {
             string selectedCustomer = CustomerFilter.SelectedItem.ToString();
-
-            MessageBox.Show($"Filtering by {selectedCustomer}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            // DEBUG
+            //MessageBox.Show($"Filtering by {selectedCustomer}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
 
             // load data from database
             // SQL query: SELECT * FROM Orders WHERE Customer = selectedCustomer
+            if (selectedCustomer == "All Customers")
+            {
+                LoadOrders(lastMonthOnly: LastMonthFilter.IsChecked == true);
+            }
+            else
+            {
+                LoadOrders(selectedCustomer, lastMonthOnly: LastMonthFilter.IsChecked == true);
+            }
         }
     }
 
@@ -289,6 +440,10 @@ public partial class MainWindow : Window
 
         // load data from database
         // SQL query: SELECT * FROM Orders WHERE OrderDate >= DATEADD(month, -1, GETDATE())
+        LoadOrders(
+            customerId: CustomerFilter.SelectedItem?.ToString() == "All Customers" ? null : CustomerFilter.SelectedItem?.ToString(),
+            lastMonthOnly: LastMonthFilter.IsChecked == true
+            );
     }
 
     private void OrdersGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -306,14 +461,10 @@ public partial class MainWindow : Window
 
     private void CountryFilter_TextChanged(object sender, TextChangedEventArgs e)
     {
-        string inputStr = CountryFilter.Text;
+        // add text field apply action before executing query
 
-        // add text field enter action before executing query
-
-        MessageBox.Show($"Filtering by {inputStr}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
-   
-        // load data from database
-        // SQL query: SELECT * FROM Customers WHERE Country LIKE '%{inputStr}%'
+        // DEBUG
+        //MessageBox.Show($"Filtering by {inputStr}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
     }
 
     private void CustomersGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -388,5 +539,40 @@ public partial class MainWindow : Window
         {
             MessageBox.Show("Select connection", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
         }
+    }
+
+    private void Button_Click_RefreshProducts(object sender, RoutedEventArgs e)
+    {
+        CategoryFilter.SelectedValue = null;
+        LoadProducts(); 
+    }
+
+    private void Button_Click_RefreshOrders(object sender, RoutedEventArgs e)
+    {
+        CustomerFilter.SelectedItem = "All Customers";
+        LastMonthFilter.ClearValue(CheckBox.IsCheckedProperty);
+        LoadOrders(); 
+    }
+
+    private void ButtonApplyFilter_Click_Apply(object sender, RoutedEventArgs e)
+    {
+        string inputStr = CountryFilter.Text.Trim();
+
+        // load data from database
+        // SQL query: SELECT * FROM Customers WHERE Country LIKE '%{inputStr}%'
+        if (string.IsNullOrWhiteSpace(inputStr))
+        {
+            LoadCustomers();
+        }
+        else
+        {
+            LoadCustomers(inputStr);
+        }
+    }
+
+    private void Button_Click_RefreshCustomers(object sender, RoutedEventArgs e)
+    {
+        CountryFilter.Text = "";
+        LoadCustomers(); // TEMP ignore filter box DONE
     }
 }
